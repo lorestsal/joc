9d51a1b42fffa1da9bf75df2d4a8a1f310244828
guaca <estela.francoc@gmail.com>
2015-11-20 19:28:02 +0100

    setup
    pujada de l’estructura del projecte

---
+++ src/ioc/m3/uf2/eac6/Eac6.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ioc.m3.uf2.eac6;
+
+/**
+ *
+ * @author estelafranco
+ */
+public class Eac6 {
+    
+}
