f408ca6e5ada9eab3c7401f30f7315bb7e5d0747
guaca <estela.francoc@gmail.com>
2015-11-29 20:35:38 +0100

    actualització joc1

--- src/ioc/m3/uf2/eac6/Menu.java
+++ src/ioc/m3/uf2/eac6/Menu.java
@@ -27,26 +27,34 @@ public class Menu {
             Entrada opcio = new Entrada();
             menu.mostrarMenu(MENU_INCIAL);
             int op=opcio.entrarEnter("Entra un enter: ");
-            switch(op)
-            {
-                case 1:
-                    //Joc1();
-                    ExecucioJoc1 joc1 = new ExecucioJoc1();
-                    joc1.joc1(paraules.diccionari);
-                break;
-                case 2:
-                    //Joc2();
-                break;                    
-                default:
-                {
-                    System.out.println();                   
-                    System.out.println("Només s'admeten dues opcions. Permeteu recordar-vos-les:");
-                    System.out.println();
-                    System.out.println("======================================================================");
-                    //presentacio();
-                };
+            while (op!=6){
+                switch(op)
+                    {
+                    case 1:
+                        ExecucioJoc1 joc1 = new ExecucioJoc1();
+                        joc1.joc1();
+                    break;
+                    case 2:
+                        //Joc2();
+                    break; 
+                    case 6:
+                        System.out.println("Fins la propera!");
+                    break;
+                    default:
+                    {
+                        System.out.println();                   
+                        System.out.println("Només s'admeten tres opcions. Permeteu recordar-vos-les:");
+                        System.out.println();
+                        menu.mostrarMenu(MENU_INCIAL);
+                        op = opcio.entrarEnter("Entra un enter: ");
+                    };
+                }
+            // Un cop finalitzats els jocs, tornem a oferir escollir joc o sortir
+            menu.mostrarMenu(MENU_INCIAL);
+            op = opcio.entrarEnter("Entra un enter: ");    
+            
             }
             
         }
-   
+        
 }
