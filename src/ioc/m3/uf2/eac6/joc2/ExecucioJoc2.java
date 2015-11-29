243e4bf1a264ba798a649728f42c6330ea6f779d
Salva <Salva@Topstar>
2015-11-29 12:57:34 +0100

    Adaptación de las funciones del A408 v1

--- src/ioc/m3/uf2/eac6/joc2/ExecucioJoc2.java
+++ src/ioc/m3/uf2/eac6/joc2/ExecucioJoc2.java
@@ -17,27 +17,110 @@ public class ExecucioJoc2 {
      * @param dipositLletres es l'array de lletres válides de la partida
      * @return un boleà que indica si la proposta es vàlida
      */
-    public boolean validarProposta(String [] propostaParaules, char [] dispositLletres){
+    private boolean validarProposta(String[] propostaParaules, char[] dipositLletres){
+        boolean ret=true;
+        boolean[] marques = new boolean[dipositLletres.length];
+        for(int i=0; ret && i<propostaParaules.length; i++){
+            ret = validarParaula(propostaParaules[i], dipositLletres, marques);
+        }
+        return ret;
+    }
+    
+        /**
+     * Indica si totes les lletres de la paraula passada en el primer 
+     * parÃ metres es troben contingudes dins el diposit (segon parÃ metre).
+     * @param paraules Ã©s la paraula a comprovar.
+     * @param diposit Ã©s el diposit de lletres on comprovar     
+     * @param amagades Ã©s una colÂ·lecciÃ³ de valors lÃ²gics que indica si anteriorment
+     * ja s'ha trobat una correspondÃ¨ncia amb la lletra que es troba a la mateixa 
+     * posiciÃ³ que el valor i per tant no Ã©s pot considerar un lletra vÃ lida.
+     * @return si la comprovaciÃ³ Ã©s o no correcta.
+     */
+    private boolean validarParaula(String paraula, char[] diposit, boolean[] amagades){
+        boolean ret=true;
+        for(int j=0; ret && j<paraula.length(); j++){
+            ret = validaLletra(paraula.charAt(j), diposit, amagades);
+        }        
+        return ret;
+    }
+    /**
+     * Indica si la lletra de la paraula passada en el primer 
+     * parÃ metre es troba continguda dins el dipÃ²sit (segon parÃ metre).
+     * @param lletra Ã©s la lletra a comprovar
+     * @param diposit Ã©s el diposit de lletres on comprovar     
+     * @param amagades Ã©s una colÂ·lecciÃ³ de valors lÃ²gics que indica si anteriorment
+     * ja s'ha trobat una correspondÃ¨ncia amb la lletra que es troba a la mateixa 
+     * posiciÃ³ que el valor i per tant no Ã©s pot considerar un lletra vÃ lida.
+     * @return si la comprovaciÃ³ Ã©s o no correcta.
+     */
+    private boolean validaLletra(char lletra, char[] diposit, boolean[] amagades){
         boolean ret=false;
-        //A CODIFICAR... 
+        int pos=0;
+        while(!ret && pos<diposit.length){
+            if(!amagades[pos] && diposit[pos]==lletra){
+                ret = true;
+                amagades[pos]=true;
+            }
+            pos++;
+        }
         return ret;
     }
     
     /**
-     * Compara les paraules de la cadena amb les paraules del diccinari
-     * Crear un conjunt buit de paraules vàlides on afegir les paraules que es vagin validant
-     * Per cada paraula del conjunt de paraules afegir-la al conjunt de paraules vàlides o descartar-la.
-     * Cercar	la paraula al diccionari.
-     * Si es troba al diccionari afegir la paraula al conjunt de paraules vàlides
-     * Actualitzar puntuacions
-     * 
-     * @param paraulesProposades son les paraules a recercar al diccionari
-     * @param diccionari es el diccionari de paraules
-     * @param paraulesDescobertes array de paraules descobertes per l'usuari
+     * Troba i retorna quines paraules, d'un conjunt de paraules (primer 
+     * parÃ metre) sÃ³n en un diccionari (segon parÃ metre).
+     * @param paraules Ã©s el conjunt de paraules a verificar
+     * @param diccionari Ã©s el diccionari on fer la verificaciÃ³
+     * @return nomÃ©s el subconjunt de paraules que es troba dins el diccionari.
      */
-    public void comparaDiccionari(String [] paraulesProposades, String [] diccionari, String paraulesDescobertes []){
-        //A CODIFICAR... 
+    private String[] comparaDiccionari(String[] paraules, String[] diccionari){
+        String[] ret = new String[paraules.length]; //com a mÃ xim hi hauran paraules.length paraules correctes.
+        int mida=0;
+        for(int i=0; i<paraules.length; i++){
+            if(existeixParaula(paraules[i], diccionari)){
+                ret[mida]=paraules[i];
+                mida++;
+            }
+        }
+        
+        ret = redimensionaColleccioStrings(ret, mida);
+        return ret;
     }
+
+    /**
+     * Comprova si la paraula passada en primer lloc Ã©s dins el diccionari 
+     * (segon parÃ metre).
+     * @param paraula Ã©s la paraula a comprovar
+     * @param diccionari Ã©s le diccionari on comprovar
+     * @return cert si la paraula es troba al diccionari i fals en cas contrari.
+     */
+    private boolean existeixParaula(String paraula, String[] diccionari){
+        boolean ret=false;
+        int ind=0;
+        while(!ret && ind<diccionari.length){
+            ret= diccionari[ind]!=null && diccionari[ind].equalsIgnoreCase(paraula);
+            ++ind;
+        }
+        return ret;
+    }
+    
+     /**
+     * Redimensiona la mida de la  colecciÃ³ de les paraules a la mida indicada 
+     * en el parÃ metre de la segona posiciÃ³.
+     * @param paraules Ã©s la colÂ·lecciÃ³ de paraules a redimensionar
+     * @param mida Ã©s la mida a la que es desitja la colÂ·lecciÃ³.
+     * @return retorna la nova colÂ·lecciÃ³ de la mida correcta.
+     */
+     public String[] redimensionaColleccioStrings(String[] paraules, int mida){
+        String[] ret = new String[mida];
+        for(int i=0; i<mida && i<paraules.length; i++){
+            ret[i]=paraules[i];
+        }
+        return ret;
+    }
+    
+    
+    
     
     /**
      * Actualitza els punts de la partida
--- src/ioc/m3/uf2/eac6/joc2/IniciaJoc2.java
+++ src/ioc/m3/uf2/eac6/joc2/IniciaJoc2.java
@@ -9,32 +9,106 @@ import ioc.m3.uf2.biblioteca.*;
 public class IniciaJoc2 {
     
     /**
-     * Realitza les operacions necessaries per inicialitzar el joc,
-     * és a dir, extreure les paraules del diccionari i convertir-les
-     * en un dipòsit de lletres
-     * 
-     * @param diccionari array amb el conjunt de paraules
-     * @param mida és la mida amb la que inicialitzarem l'array per poder començar a 
-     * treballar amb ell (serà la constant MIDA_DIPOSIT)
-     * @return paraules array amb les paraules extretes
+     * Obté un nombre indeterminat de paraules a l'atzar, el nombre de lletres 
+     * de les quals suma el valor indiocat al parámetre totalLletres
+     * @param diccionari array de paraules d'on extreure les paraules a 
+     * l'atzar (diccionari).
+     * @param totalLletres nombre de lletres que caldrà acumular durant la 
+     * selecció de les paraules.
+     * @return array de paraules seleccionades per jugar la partida
      */
-    public String[] extreureParaules(String [] diccionari, int mida){
-        // Inicialitzem l'array de paraules amb mida màxima passada per paràmetre
-        String [] paraules = new String[mida];
-        // A CODIFICAR... extracció de paraules que acompleixin amb les especificacions
+    private String[] extreureParaulesDiccionari(String[] diccionari, int totalLletres){
+        String[] paraulesObtingudes = new String[totalLletres];
+        int nParaulesObtingudes=0;
+        while(totalLletres>0){
+            int pos = obtenirPosParaulaALAtzar(diccionari, totalLletres, paraulesObtingudes);
+            paraulesObtingudes[nParaulesObtingudes]=diccionari[pos];
+            totalLletres-=paraulesObtingudes[nParaulesObtingudes].length();
+            nParaulesObtingudes++;
+        }
         
-        // Retornem l'array de paraules extretes
-        return paraules;
+        return redimensionaString(paraulesObtingudes, nParaulesObtingudes);
     }
-        
+       
     /**
-     * Funció que extreu les lletres d'un array de paraules
-     * 
-     * @param paraules és el conjunt de paraules que rep la funció
-     * @param   mida és la mida que tindrà l'array (constant MIDA_DIPOSIT)
-     * @return  lletres es l'array de lletres que composaven les paraules
+     * Redimensiona la mida de la  colecció de les paraules a la mida indicada 
+     * en el paràmetre de la segona posició.
+     * @param paraules es la colecció de paraules a redimensionar
+     * @param mida ess la mida a la que es desitja la colecció.
+     * @return retorna la nova colecció de la mida correcta.
      */
+    public String[] redimensionaString(String[] paraules, int mida){
+        String[] ret = new String[mida];
+        for(int i=0; i<mida && i<paraules.length; i++){
+            ret[i]=paraules[i];
+        }
+        return ret;
+    }
+    
 
+    /**
+     * ObtÃ© la posiciÃ³ d'una paraula de la colÂ·lecciÃ³ de paraules passades per 
+     * parÃ metre que compleixi:<br/>
+     * A) que la seva mida (nombfre de lletres) no sobrepassi la quantitat 
+     * indicada per midaMaxima.<br/>
+     * B) que no existeixi a la colÂ·lecciÃ§Ã³ de paraules passada en tercera posiciÃ³
+     * @param paraules Ã©s la colÂ·lecciÃ³ d'on escolir les paraules
+     * @param midaMaxima Ã©s la mida mÃ xima que la paruala pot fer.
+     * @param ignorarEntrades sÃ³n les paraules a ignorar. Ã‰s a dir, la paraula 
+     * escollida no pot estar continguda en aquesta colÂ·lecciÃ³. CaldrÃ  ignorar 
+     * les durant la selecciÃ³.
+     * @return la posiciÃ³ de la paraula seleccionada que compleix tot l'explicat.
+     */
+    private int obtenirPosParaulaALAtzar(String[] paraules, int midaMaxima, String[] ignorarEntrades){
+        int posRandom = nanoRandom(paraules.length);
+        int i=posRandom;
+        boolean volta=false;
+        
+        while(!volta && (paraules[i].length()>midaMaxima 
+                     || hiHaParaula(paraules[i], ignorarEntrades))){            
+            i++;
+            if(i==paraules.length){
+                i=0;
+            }
+            volta = i==posRandom;
+        }
+        if(volta){
+            i=-1;
+        }
+        return i;
+    }
+    
+    /**
+     * ObtÃ© i retorna un valor random de tipus enter, el qual oscilÂ·larÃ  entre 
+     * el valor zero (inclÃ²s) i el valor limitValue (no inclÃ²s).
+     * @param limitValue Ã©s el valor lÃ­mit superior del rang retornat per la funciÃ³
+     * @return 
+     */
+    private int nanoRandom(int limitValue){
+        long nano = System.nanoTime();
+        int ret = (int) (nano%limitValue);
+        if(ret<0){
+            ret*=-1;
+        }
+        return ret;
+    }
+    
+    /**
+     * Comprova si la paruala passada per parÃ metre es troba a la llista de 
+     * parules del segon parÃ metre
+     * @param paraula Ã©s paraula a cercar
+     * @param paraules Ã©s la llistra on cercar
+     * @return 
+     */
+     private boolean hiHaParaula(String paraula, String[] paraules){
+        boolean ret=false;
+        int ind=0;
+        while(!ret && ind<paraules.length){
+            ret= paraules[ind]!=null && paraules[ind].equalsIgnoreCase(paraula);
+            ++ind;
+        }
+        return ret;
+    }
     
 
    
