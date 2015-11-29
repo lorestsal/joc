8f0148226d2f36ff3355c92ab5765f4a73095071
guaca <estela.francoc@gmail.com>
2015-11-29 13:21:08 +0100

    elimino declaració funció actualitzar diposit d'execuciójoc2 pq ja la tenim implementada en utilitats

--- src/ioc/m3/uf2/eac6/joc2/ExecucioJoc2.java
+++ src/ioc/m3/uf2/eac6/joc2/ExecucioJoc2.java
@@ -25,16 +25,16 @@ public class ExecucioJoc2 {
         }
         return ret;
     }
-    
-        /**
+       
+    /**
      * Indica si totes les lletres de la paraula passada en el primer 
-     * parÃ metres es troben contingudes dins el diposit (segon parÃ metre).
-     * @param paraules Ã©s la paraula a comprovar.
-     * @param diposit Ã©s el diposit de lletres on comprovar     
-     * @param amagades Ã©s una colÂ·lecciÃ³ de valors lÃ²gics que indica si anteriorment
-     * ja s'ha trobat una correspondÃ¨ncia amb la lletra que es troba a la mateixa 
-     * posiciÃ³ que el valor i per tant no Ã©s pot considerar un lletra vÃ lida.
-     * @return si la comprovaciÃ³ Ã©s o no correcta.
+     * paràmetres es troben contingudes dins el diposit (segon paràmetre).
+     * @param paraula la paraula a comprovar.
+     * @param diposit el diposit de lletres on comprovar     
+     * @param amagades un array de valors lògics que indica si anteriorment
+     * ja s'ha trobat una correspondencia amb la lletra que es troba a la mateixa 
+     * posició que el valor i per tant no es pot considerar un lletra vàlida.
+     * @return si la comprovació és o no correcta.
      */
     private boolean validarParaula(String paraula, char[] diposit, boolean[] amagades){
         boolean ret=true;
@@ -45,13 +45,13 @@ public class ExecucioJoc2 {
     }
     /**
      * Indica si la lletra de la paraula passada en el primer 
-     * parÃ metre es troba continguda dins el dipÃ²sit (segon parÃ metre).
-     * @param lletra Ã©s la lletra a comprovar
-     * @param diposit Ã©s el diposit de lletres on comprovar     
-     * @param amagades Ã©s una colÂ·lecciÃ³ de valors lÃ²gics que indica si anteriorment
-     * ja s'ha trobat una correspondÃ¨ncia amb la lletra que es troba a la mateixa 
-     * posiciÃ³ que el valor i per tant no Ã©s pot considerar un lletra vÃ lida.
-     * @return si la comprovaciÃ³ Ã©s o no correcta.
+     * parÃ metre es troba continguda dins el diposit (segon parametre).
+     * @param lletra la lletra a comprovar
+     * @param diposit el diposit de lletres on comprovar     
+     * @param amagades un array de valors lògics que indica si anteriorment
+     * ja s'ha trobat una correspondencia amb la lletra que es troba a la mateixa 
+     * posició que el valor i per tant no es pot considerar un lletra vàlida.
+     * @return si la comprovació és o no correcta.
      */
     private boolean validaLletra(char lletra, char[] diposit, boolean[] amagades){
         boolean ret=false;
@@ -68,13 +68,13 @@ public class ExecucioJoc2 {
     
     /**
      * Troba i retorna quines paraules, d'un conjunt de paraules (primer 
-     * parÃ metre) sÃ³n en un diccionari (segon parÃ metre).
-     * @param paraules Ã©s el conjunt de paraules a verificar
-     * @param diccionari Ã©s el diccionari on fer la verificaciÃ³
-     * @return nomÃ©s el subconjunt de paraules que es troba dins el diccionari.
+     * paràmetre) son en un diccionari (segon paràmetre).
+     * @param paraules el conjunt de paraules a verificar
+     * @param diccionari el diccionari on fer la verificació
+     * @return el subconjunt de paraules que es troba dins el diccionari.
      */
     private String[] comparaDiccionari(String[] paraules, String[] diccionari){
-        String[] ret = new String[paraules.length]; //com a mÃ xim hi hauran paraules.length paraules correctes.
+        String[] ret = new String[paraules.length]; //com a màxim hi hauran paraules.length paraules correctes.
         int mida=0;
         for(int i=0; i<paraules.length; i++){
             if(existeixParaula(paraules[i], diccionari)){
@@ -88,10 +88,10 @@ public class ExecucioJoc2 {
     }
 
     /**
-     * Comprova si la paraula passada en primer lloc Ã©s dins el diccionari 
-     * (segon parÃ metre).
-     * @param paraula Ã©s la paraula a comprovar
-     * @param diccionari Ã©s le diccionari on comprovar
+     * Comprova si la paraula passada en primer lloc és dins el diccionari 
+     * (segon paràmetre).
+     * @param paraula la paraula a comprovar
+     * @param diccionari el diccionari on comprovar
      * @return cert si la paraula es troba al diccionari i fals en cas contrari.
      */
     private boolean existeixParaula(String paraula, String[] diccionari){
@@ -105,11 +105,11 @@ public class ExecucioJoc2 {
     }
     
      /**
-     * Redimensiona la mida de la  colecciÃ³ de les paraules a la mida indicada 
-     * en el parÃ metre de la segona posiciÃ³.
-     * @param paraules Ã©s la colÂ·lecciÃ³ de paraules a redimensionar
-     * @param mida Ã©s la mida a la que es desitja la colÂ·lecciÃ³.
-     * @return retorna la nova colÂ·lecciÃ³ de la mida correcta.
+     * Redimensiona la mida de la colecció de les paraules a la mida indicada 
+     * en el paràmetre de la segona posició.
+     * @param paraules array de paraules a redimensionar
+     * @param mida la mida a la que es desitja la colÂ·lecció.
+     * @return retorna array de la mida correcta.
      */
      public String[] redimensionaColleccioStrings(String[] paraules, int mida){
         String[] ret = new String[mida];
@@ -118,8 +118,6 @@ public class ExecucioJoc2 {
         }
         return ret;
     }
-    
-    
     
     
     /**
@@ -138,22 +136,6 @@ public class ExecucioJoc2 {
         
     }
     
-    /**
-     * Actualitza les lletres del diposit
-     * 
-     * Recorre totes les paraules actualitzant el dipòsit amb les lletres de les paraules.
-     * Per cada paraula actualitzar el dipòsit amb les seves lletres.
-     * Recorre totes les lletres de la paraula actualitzant el dipòsit
-     * Per cada lletra cercar en el dipòsit una lletra idèntica i eliminar-la, substituint-la per un espai en blanc.
-     * 
-     * @param dipositLletres es l'array de lletres válides de la partida
-     * @param paraulesDescobertes array de paraules descobertes per l'usuari
-     * 
-     */
-    public void actualitzaLletres(char dipositLletres [], String paraulesDescobertes []){
-        //A CODIFICAR... 
-        
-    }
 }
 
 
